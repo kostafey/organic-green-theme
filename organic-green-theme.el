a4d90780b13a265a61ffbc595abaf93f53cba519
Kostafey <kostafey@gmail.com>
2014-02-04 14:30:16 +0400

    Add fill-column-indicator color.

--- organic-green-theme.el
+++ organic-green-theme.el
@@ -210,10 +210,15 @@ Basic, Font Lock, Isearch, Jabber, rst, magit, Web faces are included.")
 
 (custom-theme-set-variables
  'organic-green
+
+ ;; lisp parentheses rainbow
  `(hl-paren-colors '("#326B6B"))
  `(hl-paren-background-colors
    '("#00FF99" "#CCFF99" "#FFCC99" "#FF9999" "#FF99CC"
-     "#CC99FF" "#9999FF" "#99CCFF" "#99FFCC" "#7FFF00")))
+     "#CC99FF" "#9999FF" "#99CCFF" "#99FFCC" "#7FFF00"))
+
+ ;; fill-column-indicator
+ `(fci-rule-color "gray80"))
 
 ;;;###autoload
 (when load-file-name
